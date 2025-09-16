90b3727bced152b6d32a661e8e03df9b695041b1
16marleni <marlenibarria82@gmail.com>
2025-09-15 19:26:52 -0500

    Laboratorio # 2 - Completo

---
+++ Laboratorio2/Laboratorio2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Laboratorio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+            //Ejemplo utilizando las variables de instancia de Clase.
+            client.FirstName = "Marleni";
+            client.LastName = "Barría";
+            client.Age = 23;
+            client.Id = 8977;
+        
+            Console.WriteLine(client.GetFullName());
+        }
+    }
+
+    public class Client
+    {
+        //Declarando variables de instancia en clase.
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ushort Age { get; set; }
+
+        public string GetFullName()
+        {
+            //Utilizando variables de instacia dentro de metodos de la clase.
+            return FirstName + " " + LastName;
+        }
+
+    }
+}
--- Laboratorio2/Laboratorio22/obj/Debug/net8.0/Laboratorio22.AssemblyInfo.cs
+++ Laboratorio2/Laboratorio22/obj/Debug/net8.0/Laboratorio22.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio 1")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Laboratorio22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ec42fc2d2672a5e3a82160e59072fd94ad4fad80")]
-[assembly: System.Reflection.AssemblyProductAttribute("Laboratorio 1")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio 1")]
+[assembly: System.Reflection.AssemblyProductAttribute("Laboratorio22")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Laboratorio22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
