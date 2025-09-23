dcbd69f748ca778c2c683fad4c24e6906532e11d
16marleni <marlenibarria82@gmail.com>
2025-09-22 20:48:47 -0500

    Completo

---
+++ Laboratorio6/Laboratorio61/Program.cs
@@ -0,0 +1,28 @@
+﻿using System; //Importa las funciones básicas .NET
+
+namespace Laboratorio62 //Para organizar el código
+{
+    class Program
+    {
+        static void Main(string[] args) //Punto de entrada del programa
+        {
+            int num;
+            Console.WriteLine("Digite el número deseado");
+
+            try
+            {
+                num = Int16.Parse(Console.ReadLine()); //Convierte la entrada en un número entero y el 16 indica que es un entero de 16 bits
+            }
+            catch (FormatException ex) //Captura el error en caso de que la entrada no sea un número
+            {
+                Console.WriteLine("No ha introducido un digito valido");
+                num = -1; //Asignamos un valor por defecto en caso de error
+            }catch (OverflowException ex) {
+                Console.WriteLine("El número introducido es muy grande");
+                num = -1; //Asignamos un valor por defecto en caso de error
+            }
+
+            Console.WriteLine(num);
+        }
+    }
+}
---
+++ Laboratorio6/Laboratorio64/Program.cs
@@ -0,0 +1,24 @@
+﻿using System; //Importa las funciones básicas .NET
+
+namespace Laboratorio64 //Para organizar el código
+{
+    class Program
+    {
+        static void checkAge(int age) //Función que valida la edad
+        {
+          if (age < 18)
+          {
+                throw new ArithmeticException("Acceso negado - No cumple con el criterio de edad"); //Lanza una excepción si la edad es menor a 18
+            }
+          else
+          {
+                Console.WriteLine("Acceso Concedido");
+          }
+        }
+
+        static void Main(string[] args)
+        {
+            checkAge(15); //Llamada a la función con un valor
+        }
+    }
+}
