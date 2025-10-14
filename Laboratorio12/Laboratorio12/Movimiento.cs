09fcfce839b6af2b7bff582002b248017dfc47af
16marleni <marlenibarria82@gmail.com>
2025-10-13 20:13:48 -0500

    Completo

---
+++ Laboratorio12/Laboratorio12/Movimiento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio12
+{
+    public class Movimiento
+    {
+        public double RVelocidad { get; set; }
+        public double RTiempo { get; set; }
+
+        public Movimiento(double Velocidad, double Tiempo)
+        {
+            RVelocidad = Velocidad;
+            RTiempo = Tiempo;
+        }
+
+        public double CalcularDistancia()
+        {
+            return RVelocidad * RTiempo;
+        }
+    }
+}
