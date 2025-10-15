ac82cc1ad2be5545179e1d1d975cc6b26e85f084
16marleni <marlenibarria82@gmail.com>
2025-10-15 18:27:57 -0500

    Completo

---
+++ Laboratorio12/Laboratorio122/Promedio.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Laboratorio122
+{
+    internal class Promedio
+    {
+        public double RNota1 { get; set; }
+        public double RNota2 { get; set; }
+        public double RNota3 { get; set; }
+
+        public Promedio(double Nota1, double Nota2, double Nota3)
+        {
+            RNota1 = Nota1;
+            RNota2 = Nota2;
+            RNota3 = Nota3;
+        }
+
+        public double CalcularPromedio()
+        {
+            return (RNota1 + RNota2 + RNota3) / 3;
+        }
+    }
+}
