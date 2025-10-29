09f008ae5de775505f9480cabb5f4a0bcf0b6676
16marleni <marlenibarria82@gmail.com>
2025-10-29 17:16:48 -0500

    README inicial para la rama proyectos

--- Laboratorio10/Laboratorio10/Program.vb
+++
@@ -1,8 +0,0 @@
-Imports System 'A diferencia de C# que usa using
-
-Module Module1 'A diferencia de C# que usa namespace
-    Sub Main() 'Inicio main 
-        Console.WriteLine("Hola")
-        Console.WriteLine("Este es mi primer programa de consola")
-    End Sub
-End Module
